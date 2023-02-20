0bf93e259ce27b7ee90689f8a86f60e7f996b886
Nidhi singh <117963273+nidhiii112@users.noreply.github.com>
2023-02-20 21:58:27 +0530

    Update assign12.java

--- assign12.java
+++ assign12.java
@@ -1,11 +1,13 @@
 //question(12)
 import java.util.Scanner;
-public class assign12{
-    public static void main(String[] args) {
+public class assign12
+{
+    public static void main(String[] args) 
+    {
         Scanner input = new Scanner(System.in);
         int a= 12;
         int b= 11;
         int c = a+b;
         System.out.println("sum = "+a+"+"+b+"="+c);
     }
-    }   
+}   
