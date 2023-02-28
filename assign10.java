59012c72329963a53ff738dbd0bc6e7cf5781d9f
Nidhi singh <117963273+nidhiii112@users.noreply.github.com>
2023-02-28 18:49:15 +0530

    Update assign10.java

--- assign10.java
+++ assign10.java
@@ -4,7 +4,7 @@ public class assign10{
     public static void main(String[] args) { 
         Scanner input = new Scanner(System.in); 
         System.out.println("find the value :");
-        float a= 5%(5*2)-3+6/2f;
+        float a= 5%(5*2)-3+6/2f; 
         System.out.println(a);   
     } 
  }        
