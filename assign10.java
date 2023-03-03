52efd26beb695ddea78b1f59d7647ae370dc30c0
Nidhi singh <117963273+nidhiii112@users.noreply.github.com>
2023-03-03 11:18:36 +0530

    Update assign10.java

--- assign10.java
+++ assign10.java
@@ -1,5 +1,5 @@
 //question(10)
-import java.util.Scanner;
+import java.util.Scanner; 
 public class assign10{ 
     public static void main(String[] args) { 
         Scanner input = new Scanner(System.in); 
