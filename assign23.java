10f418ceaaca9f7ae5c27c4ba0928f2617996050
Nidhi singh <117963273+nidhiii112@users.noreply.github.com>
2023-03-08 14:03:54 +0530

    Update assign23.java

--- assign23.java
+++ assign23.java
@@ -1,7 +1,9 @@
 //question(23)
 import java.util.Scanner;
-public class assign23{
-    public static void main(String[] args) {
+public class assign23
+{
+    public static void main(String[] args) 
+    {
         Scanner obj = new Scanner(System.in);
         System.out.println("enter the integer");
         int a = obj.nextInt();
