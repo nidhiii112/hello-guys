f71df49e0d368e529a5ba31865d597fa98a73738
Nidhi singh <117963273+nidhiii112@users.noreply.github.com>
2023-03-09 12:31:07 +0530

    Update assign13.java

--- assign13.java
+++ assign13.java
@@ -1,7 +1,9 @@
 //question(13)
 import java.util.Scanner;
-public class assign13{
-    public static void main(String[] args) {
+public class assign13
+{
+    public static void main(String[] args)
+ {
         Scanner input = new Scanner(System.in);
         int a= 2;
         int b= 4;
