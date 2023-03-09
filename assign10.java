ecedb5519b6780d9eb8dcb4d3154eb05874f9fbe
Nidhi singh <117963273+nidhiii112@users.noreply.github.com>
2023-03-09 16:39:22 +0530

    10th commit

--- assign10.java
+++ assign10.java
@@ -1,7 +1,9 @@
 //question(10)
 import java.util.Scanner; 
-public class assign10{ 
-    public static void main(String[] args) {  
+public class assign10
+{ 
+    public static void main(String[] args) 
+    {  
         Scanner input = new Scanner(System.in); 
         System.out.println("find the value :");  
         float a= 5%(5*2)-3+6/2f;   
