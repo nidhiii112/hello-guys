b096181081b0e48d2399a725f6652a9e7bd731d7
Nidhi singh <117963273+nidhiii112@users.noreply.github.com>
2024-01-05 20:43:41 +0530

    Update assign1.java

--- assign1.java
+++ assign1.java
@@ -3,7 +3,7 @@
 import java.util.Scanner;  
 public class assign1
 { 
-    public static void main(String[] args)  
+    public static void main(String[] args)   
     {  
         Scanner input = new Scanner(System.in);   
         System.out.println("Enter your name :");  
